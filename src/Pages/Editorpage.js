0891b0a25c8990c10ca14de0ae5511139cb05a0b
Abir Pal <abirpal282002@gmail.com>
2022-03-27 04:24:11 +0530

    Side panel of Edito done

--- src/Pages/Editorpage.js
+++ src/Pages/Editorpage.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import Client from "../Components/Client";
+import Editor from "../Components/Editor";
+import './Style/Editorpage.css'
 
 const Editorpage = () => {
+  const [client, setClient] = useState([
+    { socketId: 1, username: "Abir Pal" },
+    { socketId: 2, username: "Saikat Mukherjee" },
+    { socketId: 3, username: "Pranay Gupta" },
+    { socketId: 4, username: "Souvik Mandal" },
+    { socketId: 5, username: "Soumya Banerjee" },
+  ]);
   return (
-    <div>Editor Page </div>
-  )
-}
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <h2 className="display-2"> &lt; Code Real Time / &gt;</h2>
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+          {
+              client.map(client => (<Client key={client.socketId} username={client.username}/>))
+          }
+          </div>
+          <button className="btn copy">Copy Room Id</button>
+          <button className="btn leave">Leave Button</button>
+        </div>
+      </div>
 
-export default Editorpage
+      <div className="editorWrap">
+          <Editor />
+      </div>
+    </div>
+  );
+};
+
+export default Editorpage;
